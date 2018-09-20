0fad52ad53f317709b445339a901874e0df9295d
caiwenwena <1875506684@qq.com>
2018-09-20 11:33:34 +0800

    、

---
+++ js/index.20180920113323.js
@@ -0,0 +1,9 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/shi-di-zi-636.jpeg' : 'images/shi-di-zi-318.jpeg');};
+if(!window.HTMLPictureElement){r();}
+
+});
